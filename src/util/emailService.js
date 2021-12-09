803eeba18962d31834c15c565b1f2ccaf3060a34
DavidRaj023 <benjamindavid023@gmail.com>
2021-12-09 11:37:06 +0530

    Updated Email View

--- src/util/emailService.js
+++ src/util/emailService.js
@@ -2,19 +2,19 @@ require('dotenv').config();
 const sgMail = require('@sendgrid/mail');
 const sendGridAPI = process.env.SEND_GRID;
 
-const sendMail = (user) =>{
+const sendMail = async(user) =>{
     try {
         const id = user._id.toString();
         const userName = user.name;
         const email = user.email;
-        const link = 'http://localhost:3000/api/v1/user/account?id=' + id + '&email=' + email;
+        const link = 'http://localhost:3000/api/v1/user/account?id=' + id;
         
         sgMail.setApiKey(sendGridAPI);
         sgMail.send({
             to: email,
             from: 'benjamindavid023@gmail.com',
             subject: 'Mail From User Register Application',
-            text: 'Hi ' + userName + ',  Please click the following link to register your email: ' + link
+            html: '<h3>Hi ' + userName + ', </h3>' + '<p>Please click the following link to register your email: ' + link +' </p>'
         });
 
         console.log('Link sent to this Email ' + email);
